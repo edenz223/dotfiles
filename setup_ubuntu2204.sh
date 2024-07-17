d85325d874e9a8b2684b324ba82192c3e93b0be5
edenz223 <edenz223@outlook.com>
2024-07-18 03:13:09 +0800

    more changes

--- setup_ubuntu2204.sh
+++ setup_ubuntu2204.sh
@@ -173,5 +173,17 @@ else
     git clone --depth 1 https://github.com/edenz223/neovimConfig "$NVIM_CONFIG_DIR"
 fi
 
+# installl gdu for disk usage analyzer
+curl -L https://github.com/dundee/gdu/releases/latest/download/gdu_linux_amd64.tgz | tar xz
+chmod +x gdu_linux_amd64
+sudo mv gdu_linux_amd64 /usr/bin/gdu
+
+# install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm -rf lazygit*
+
 echo "Finished installing Nvim and its dependencies!"
 
